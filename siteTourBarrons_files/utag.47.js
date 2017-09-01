1a7deb17ecb9ec7085e9b7a11efc25af32daf0a3
Salvador Elizarraras Montenegro <salvadorbfm@gmail.com>
2017-09-01 10:31:48 -0500

    First commit

---
+++ siteTourBarrons_files/utag.47.js
@@ -0,0 +1,6 @@
+//tealium universal tag - utag.47 ut4.0.201706052108, Copyright 2017 Tealium.com Inc. All Rights Reserved.
+try{(function(id,loader,u){try{u=utag.o[loader].sender[id]={}}catch(e){u=utag.sender[id]};u.ev={'view':1};u.t='@@',u.i=[],u.p=["//dbg52463.moatads.com?a=0929dd24da934b34a99983e5a36b7e81","//d5i9o0tpq9sa1.cloudfront.net?a=0929dd24da934b34a99983e5a36b7e81","","","","","",""];u.cachebust="disabled";u.cachevar=""||"_rnd";u.map={};u.extend=[];u.send=function(a,b,c,d,e,f){if(u.ev[a]){for(c=0;c<u.p.length;c++){if(u.p[c]!=""){var x=u.rp(u.p[c],b);if(x!=""){if(u.cachebust=="enabled"&&x.indexOf("_rnd")<0){x+="&"+u.cachevar+"="+Math.random();}
+var img=new Image();img.src=x;u.i.push(img);}}}}}
+u.rp=function(a,b){if(typeof a!="undefined"&&a.indexOf(u.t)>0){a=a.replace(/@@([^@]+)@@/g,function(m,d){if(b[d]){return encodeURIComponent(b[d]);}else{return'';}});}
+return a;}
+try{utag.o[loader].loader.LOAD(id)}catch(e){utag.loader.LOAD(id)}})('47','wsjdn.barrons-former');}catch(error){utag.DB(error);}
